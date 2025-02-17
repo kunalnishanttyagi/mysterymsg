b2c989d9fa2b306056d8e1d00c694b365e405373
Nishant Tyagi <kunalnishanttyagi@gmail.com>
2025-02-17 23:56:46 +0530

    worked more on ui of user

--- src/app/components/welcomeuser.tsx
+++ src/app/components/welcomeuser.tsx
@@ -1,7 +1,7 @@
 "use client";
 // import React from "react";
 import  Button  from "./ui/moving-border";
-import React, { useState } from "react";
+import React, { cloneElement, useState } from "react";
 import { HoverEffect } from "./ui/card-hover-effect";
  
 // import React from "react";
@@ -11,6 +11,8 @@ import { userdatainterface } from "../user/[username]/page";
 // import React from "react";
       import { BackgroundLines } from "../components/ui/background-lines";
       import ToggleSwitch from "./ui/toggler";
+import Link from "next/link";
+import { buttons } from "./ui/Buttons";
       // interface ToggleSwitchProps {
       //   isOn: boolean;
       //   onToggle: () => void;
@@ -23,21 +25,50 @@ interface UserProps {
       
     const BackgroundLinesDemo: React.FC<UserProps> = ({ data }) => {
       const [isOn, setIsOn] = useState(false);
+   const button = buttons.find((btn) => btn.name === "Shimmer");
    
+     if (!button) {
+       return <p>Button not found</p>;
+     }
+     const [showmessage,setshowmessage]=useState(true);
       return (
           <BackgroundLines className=" relative  -z-50 flex items-center justify-center w-full flex-col px-4">
             
+            <div className="absolute text-[40px] left-[110px] top-[48px]" >{data.firstname+" "+ data.lastname} </div>
             <div className=" z-50 flex justify-center items-center h-full w-full " >
-            
 
                 <div className=" mt-[200px] pl-[100px]  h-auto w-[1200px] flex " > 
                         
                       <div className= "flex flex-col">
                       <ToggleSwitch  isOn={isOn} onToggle={() => setIsOn(!isOn)} />
-                      <div className=" text-[30px] mt-4  pl-[40px] " >What people say you!</div>
-                      <div className="max-w-5xl mx-auto px-8">
-                        <HoverEffect items={projects} />
-                      </div>
+                        
+                          {/* // here i want to use if else conditional rendering  */}
+                          {
+                            showmessage ? 
+                               (<div>
+                                <div className=" flex gap-[500px]" >
+                                  <div className=" text-[30px] mt-4  pl-[40px] " >What people say you!</div>
+                                  {/* <b>Send Msg</b> */}
+                                  <button onClick={()=> setshowmessage(!showmessage)}>{cloneElement(button.component, {}, "Send Msg")}</button>
+                                  
+                                </div>
+        
+                              <div className="max-w-5xl mx-auto px-8">
+                                <HoverEffect items={projects} />
+                              </div>
+                                </div>) : ((<div className=" mt-5 pl-4">
+                                <div className=" flex gap-[500px]" >
+                                  {/* <div className=" text-[30px] mt-4  pl-[40px] " >What people say you!</div> */}
+                                  {/* <b>Send Msg</b> */}
+                                  <button onClick={()=> setshowmessage(!showmessage)}>{cloneElement(button.component, {}, "Show Msg")}</button>
+                                  
+                                </div>
+        
+                              
+                                </div>))
+                            
+                          }
+                          
                         
                       </div>
                 </div>
@@ -83,41 +114,41 @@ interface UserProps {
             "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
           link: "https://microsoft.com",
         },
-      //   {
-      //     title: "Stripe",
-      //     description:
-      //       "A technology company that builds economic infrastructure for the internet.",
-      //     link: "https://stripe.com",
-      //   },
-      //   {
-      //     title: "Netflix",
-      //     description:
-      //       "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
-      //     link: "https://netflix.com",
-      //   },
-      //   {
-      //     title: "Google",
-      //     description:
-      //       "A multinational technology company that specializes in Internet-related services and products.",
-      //     link: "https://google.com",
-      //   },
-      //   {
-      //     title: "Meta",
-      //     description:
-      //       "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
-      //     link: "https://meta.com",
-      //   },
-      //   {
-      //     title: "Amazon",
-      //     description:
-      //       "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
-      //     link: "https://amazon.com",
-      //   },
-      //   {
-      //     title: "Microsoft",
-      //     description:
-      //       "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
-      //     link: "https://microsoft.com",
-      //   },
+        {
+          title: "Stripe",
+          description:
+            "A technology company that builds economic infrastructure for the internet.",
+          link: "https://stripe.com",
+        },
+        {
+          title: "Netflix",
+          description:
+            "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+          link: "https://netflix.com",
+        },
+        {
+          title: "Google",
+          description:
+            "A multinational technology company that specializes in Internet-related services and products.",
+          link: "https://google.com",
+        },
+        {
+          title: "Meta",
+          description:
+            "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+          link: "https://meta.com",
+        },
+        {
+          title: "Amazon",
+          description:
+            "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+          link: "https://amazon.com",
+        },
+        {
+          title: "Microsoft",
+          description:
+            "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
+          link: "https://microsoft.com",
+        },
       ];
       export default BackgroundLinesDemo;
